fd0165ee0a813d0942d10973a346a99b2e982e2f
Jcoderre <joeysteam@outlook.com>
2020-09-07 16:08:13 -0400

    Rocket Elevator Digital Presence v1.2.0

--- GenesisProgram-master/Smarty/HTML_BS4/form_sheet.js
+++ GenesisProgram-master/Smarty/HTML_BS4/form_sheet.js
@@ -1,7 +1,66 @@
 
-    $("#requestQuote").on("submit", function(event) {
-        event.preventDefault();
-        var $buildingChoice = $(this).find("[name=building]");
-        var buildingChoice = $buildingChoice.val();
-        $("#selectedBuildType").text("You choose " + buildingChoice + " as building type");
-    });
+function myFunction(){
+    var HybCheckBox = document.getElementById("HybCheckBox");
+    var HybText = document.getElementById("HybChoiceText");
+    var HybForm = document.getElementById("HybForm");
+    var AptCheckBox = document.getElementById("AptCheckBox");
+    var AptText = document.getElementById("AptChoiceText");
+    var AptForm = document.getElementById("AptForm");
+    var ComCheckBox = document.getElementById("ComCheckBox");
+    var ComText = document.getElementById("ComChoiceText");
+    var ComForm = document.getElementById("ComForm");
+    var CorpCheckBox = document.getElementById("CorpCheckBox");
+    var CorpText = document.getElementById("CorpChoiceText");
+    var CorpForm = document.getElementById("CorpForm");
+
+    if (HybCheckBox.checked == true){
+        HybText.style.display = "block";
+        HybForm.style.display = "block";
+    }   else {
+        HybText.style.display = "none"; 
+        HybForm.style.display = "none";
+    }
+
+    if (AptCheckBox.checked == true){
+        AptText.style.display = "block";
+        AptForm.style.display = "block";
+    }   else {
+        AptText.style.display = "none"; 
+        AptForm.style.display = "none";
+    }
+
+    if (ComCheckBox.checked == true){
+        ComText.style.display = "block";
+        ComForm.style.display = "block";
+    }   else {
+        ComText.style.display = "none"; 
+        ComForm.style.display = "none";
+    }
+
+    if (CorpCheckBox.checked == true){
+        CorpText.style.display = "block";
+        CorpForm.style.display = "block";
+    }   else {
+        CorpText.style.display = "none"; 
+        CorpForm.style.display = "none";
+    }
+
+}   
+
+
+$("#product-line-cost").change(function(event){
+    event.preventDefault();
+    var $input = $(this).find("input[type='radio']:checked");
+    var input = $input.val();
+    $("#cost-result").text(input);
+    console.log(input);
+})
+
+
+
+$("#max24").keyup(function() {
+    if ($(this).val() > 24){
+        $(this).val(24);
+    } 
+})
+
