282928f3d6af70eeed684ac409e7718edfbde1ca
Jcoderre <joeysteam@outlook.com>
2020-09-10 16:44:47 -0400

    Rocket Elevator Digital Presence v1.6.1

--- GenesisProgram-master/RocketElevator/HTML_BS4/form_sheet.js
+++ GenesisProgram-master/RocketElevator/HTML_BS4/form_sheet.js
@@ -60,21 +60,25 @@ $("#max24").keyup(function() {
     } 
 })
 
-    $(document).change(function(){
+
+/************* Function to calculate the total amount of elevator and the price tag **************/
+
+    $(document).on("change", function(){
         var $prod_line = $(this).find("input[type='radio'][name=OptCheck]:checked");
         var prod_line = $prod_line.val() || 0;
-        $("#cost-result").text(prod_line);
+        
         
             /********** Generate the estimated cost for Commercial use and Number of Elevator needed  **********/
 
         $("#ComForm").on("change keyup", function() {
             var $elevNum = $(this).find("[name=CageComElevatorNb]"); // Scope the Nb of elevator needed
             var elevNum = $elevNum.val();   // Set value
-            var total_Cost = elevNum * prod_line; // multiply
-
-            $("#cost-result").text(total_Cost + "$");
-            $("#elevator-needed").text(elevNum);
-            })
+            var raw_total_Cost = elevNum * prod_line; // multiply
+            var roundedCost = raw_total_Cost.toFixed(2); // fix 2 digits after comma
+            var total_Cost = Number(roundedCost);   // set to number
+            $("#cost-result").text(total_Cost + " $");
+            $("#elevator-needed").text(elevNum);                                    
+        })
 
             /*********** Generate the estimate Cost For Corporative use and Number of Elevator needed  ************/
 
@@ -85,16 +89,18 @@ $("#max24").keyup(function() {
             var nbOfBasement = $nbOfBasement.val();   
             var $nbOfFloor = $(this).find("[name=NbFloor_Corp]");   //Scope the Nb of Floor
             var nbOfFloor = $nbOfFloor.val();   
-            var nbOfStories = +nbOfFloor + +nbOfBasement; 
+            var nbOfStories = +nbOfFloor + +nbOfBasement;       //set to number
             var totalOcc = occByFloor * nbOfStories;
             var nbOfElevator = totalOcc / parseInt("1000", 10);
             var nbOfColumn = nbOfStories / parseInt("20", 10);
-            var nbOfElevByColumn = Math.ceil(nbOfElevator) / Math.ceil(nbOfColumn);
-            var totalElev = Math.ceil(nbOfElevByColumn) * Math.ceil(nbOfColumn) || 0;          
-            var total_Cost = Math.ceil(totalElev) * prod_line; // multiply
+            var nbOfElevByColumn = Math.ceil(nbOfElevator) / Math.ceil(nbOfColumn);         // Ceiling numbers to higher decimal 
+            var totalElev = Math.ceil(nbOfElevByColumn) * Math.ceil(nbOfColumn) || 0;        // ceiling number and force but 0 instead of NaN  
+            var raw_total_Cost = Math.ceil(totalElev) * prod_line; // multiply
+            var roundedCost = raw_total_Cost.toFixed(2);        // fix 2 digits after comma
+            var total_Cost = Number(roundedCost);       // set to number
 
-            $("#elevator-needed").text(Math.ceil(totalElev));
-            $("#cost-result").text(Math.ceil(total_Cost));   
+            $("#elevator-needed").text(totalElev);
+            $("#cost-result").text(total_Cost + " $");   
             })
 
             /*********** Generate the estimate Cost For Hybrid use and Number of Elevator needed  ************/
@@ -106,16 +112,18 @@ $("#max24").keyup(function() {
             var nbOfBasement = $nbOfBasement.val();   
             var $nbOfFloor = $(this).find("[name=NbFloor_Hyb]");   //Scope the Nb of Floor 
             var nbOfFloor = $nbOfFloor.val();  
-            var nbOfStories = +nbOfFloor + +nbOfBasement;
+            var nbOfStories = +nbOfFloor + +nbOfBasement;       //set to number
             var totalOcc = occByFloor * nbOfStories;
             var nbOfElevator = totalOcc / parseInt("1000", 10) ; 
             var nbOfColumn = nbOfStories / parseInt("20", 10) ;
-            var nbOfElevByColumn = Math.ceil(nbOfElevator) / Math.ceil(nbOfColumn);
-            var totalElev = Math.ceil(nbOfElevByColumn) * Math.ceil(nbOfColumn) ||0;           
-            var total_Cost = totalElev * prod_line; // multiply
+            var nbOfElevByColumn = Math.ceil(nbOfElevator) / Math.ceil(nbOfColumn);     // Ceiling numbers to higher decimal 
+            var totalElev = Math.ceil(nbOfElevByColumn) * Math.ceil(nbOfColumn) ||0;    // ceiling number and force but 0 instead of NaN         
+            var raw_total_Cost = totalElev * prod_line; // multiply
+            var roundedCost = raw_total_Cost.toFixed(2);        // fix 2 digits after comma
+            var total_Cost = Number(roundedCost);       // set to number
 
             $("#elevator-needed").text(totalElev);
-            $("#cost-result").text(Math.ceil(total_Cost));
+            $("#cost-result").text(total_Cost + " $");
             })
 
             /************  Generate the estimate Cost for Residential and the number of Elevator needed*********/
@@ -128,10 +136,12 @@ $("#max24").keyup(function() {
             var AvrgDoor = +NbOfApart / +NbOfFloor;
             var NbElev_res = Math.ceil(AvrgDoor) / 6; // Num of elevator
             var NbColumn_res = NbOfFloor / parseInt("20", 10);
-            var NbRealElev_res = Math.ceil(NbElev_res) * Math.ceil(NbColumn_res) || 0 ;
-            var totalCost = NbRealElev_res * prod_line;
+            var NbRealElev_res = Math.ceil(NbElev_res) * Math.ceil(NbColumn_res) || 0 ;     // ceiling number and force but 0 instead of NaN  
+            var raw_total_Cost = NbRealElev_res * prod_line;
+            var roundedCost  = raw_total_Cost.toFixed(2);        // fix 2 digits after comma
+            var total_Cost   = Number(roundedCost);       // set to number
         
-            $("#elevator-needed").text(Math.ceil(NbRealElev_res));
-            $("#cost-result").text(Math.ceil(totalCost));
+            $("#elevator-needed").text(NbRealElev_res);
+            $("#cost-result").text(total_Cost + " $");
         })
     })
