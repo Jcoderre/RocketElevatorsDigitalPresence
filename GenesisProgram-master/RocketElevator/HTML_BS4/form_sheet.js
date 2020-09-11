40129405b518199e4b73cfe9d343d1ac9d5e8d31
Jcoderre <joeysteam@outlook.com>
2020-09-11 11:36:33 -0400

    Rocket Elevator Digital Presence v1.6.6

--- GenesisProgram-master/RocketElevator/HTML_BS4/form_sheet.js
+++ GenesisProgram-master/RocketElevator/HTML_BS4/form_sheet.js
@@ -63,85 +63,92 @@ $("#max24").keyup(function() {
 
 /************* Function to calculate the total amount of elevator and the price tag **************/
 
-    $(document).on("change", function(){
+    $("#requestQuote").on("change", function(){
         var $prod_line = $(this).find("input[type='radio'][name=OptCheck]:checked");
         var prod_line = $prod_line.val() || 0;
         
-        
-            /********** Generate the estimated cost for Commercial use and Number of Elevator needed  **********/
+       
+                /********** Generate the estimated cost for Commercial use and Number of Elevator needed  **********/
 
-        $("#ComForm").on("change keyup", function() {
-            var $elevNum = $(this).find("[name=CageComElevatorNb]"); // Scope the Nb of elevator needed
-            var elevNum = $elevNum.val();   // Set value
-            var raw_total_Cost = elevNum * prod_line; // multiply
-            var roundedCost = raw_total_Cost.toFixed(2); // fix 2 digits after comma
-            var total_Cost = Number(roundedCost);   // set to number
-            $("#cost-result").text(total_Cost + " $");
-            $("#elevator-needed").text(elevNum);                                    
-        })
-
-            /*********** Generate the estimate Cost For Corporative use and Number of Elevator needed  ************/
-
-        $("#CorpForm").on("change keyup", function(){
-            var $occByFloor = $(this).find("[name=OccByFloor_corp]"); // Scope the Nb of Occupant By Floor
-            var occByFloor = $occByFloor.val();   
-            var $nbOfBasement = $(this).find("[name=NbBasement_corp]"); // Scope the Nb of Basement
-            var nbOfBasement = $nbOfBasement.val();   
-            var $nbOfFloor = $(this).find("[name=NbFloor_Corp]");   //Scope the Nb of Floor
-            var nbOfFloor = $nbOfFloor.val();   
-            var nbOfStories = +nbOfFloor + +nbOfBasement;       //set to number
-            var totalOcc = occByFloor * nbOfStories;
-            var nbOfElevator = totalOcc / parseInt("1000", 10);
-            var nbOfColumn = nbOfStories / parseInt("20", 10);
-            var nbOfElevByColumn = Math.ceil(nbOfElevator) / Math.ceil(nbOfColumn);         // Ceiling numbers to higher decimal 
-            var totalElev = Math.ceil(nbOfElevByColumn) * Math.ceil(nbOfColumn) || 0;        // ceiling number and force but 0 instead of NaN  
-            var raw_total_Cost = Math.ceil(totalElev) * prod_line; // multiply
-            var roundedCost = raw_total_Cost.toFixed(2);        // fix 2 digits after comma
-            var total_Cost = Number(roundedCost);       // set to number
-
-            $("#elevator-needed").text(totalElev);
-            $("#cost-result").text(total_Cost + " $");   
+            $("#ComForm").on("change keyup", function() {
+                var $elevNum = $(this).find("[name=CageComElevatorNb]"); // Scope the Nb of elevator needed
+                var elevNum = $elevNum.val();   // Set value
+                var raw_total_Cost = elevNum * prod_line; // multiply
+                var roundedCost = raw_total_Cost.toFixed(2); // fix 2 digits after comma
+                var total_Cost = Number(roundedCost);   // set to number
+                $("#cost-result").text(total_Cost + " $");
+                $("#elevator-needed").text(elevNum);                                    
             })
 
-            /*********** Generate the estimate Cost For Hybrid use and Number of Elevator needed  ************/
+                /*********** Generate the estimate Cost For Corporative use and Number of Elevator needed  ************/
 
-        $("#HybForm").on("change keyup", function(){
-            var $occByFloor = $(this).find("[name=OccByFloor_Hyb]"); // Scope the Nb of Occupant By Floor
-            var occByFloor = $occByFloor.val();   
-            var $nbOfBasement = $(this).find("[name=NbBasement_Hyb]"); // Scope the Nb of Basement
-            var nbOfBasement = $nbOfBasement.val();   
-            var $nbOfFloor = $(this).find("[name=NbFloor_Hyb]");   //Scope the Nb of Floor 
-            var nbOfFloor = $nbOfFloor.val();  
-            var nbOfStories = +nbOfFloor + +nbOfBasement;       //set to number
-            var totalOcc = occByFloor * nbOfStories;
-            var nbOfElevator = totalOcc / parseInt("1000", 10) ; 
-            var nbOfColumn = nbOfStories / parseInt("20", 10) ;
-            var nbOfElevByColumn = Math.ceil(nbOfElevator) / Math.ceil(nbOfColumn);     // Ceiling numbers to higher decimal 
-            var totalElev = Math.ceil(nbOfElevByColumn) * Math.ceil(nbOfColumn) ||0;    // ceiling number and force but 0 instead of NaN         
-            var raw_total_Cost = totalElev * prod_line; // multiply
-            var roundedCost = raw_total_Cost.toFixed(2);        // fix 2 digits after comma
-            var total_Cost = Number(roundedCost);       // set to number
+            $("#CorpForm").on("change keyup", function(){
+                var $occByFloor = $(this).find("[name=OccByFloor_corp]"); // Scope the Nb of Occupant By Floor
+                var occByFloor = $occByFloor.val();   
+                var $nbOfBasement = $(this).find("[name=NbBasement_corp]"); // Scope the Nb of Basement
+                var nbOfBasement = $nbOfBasement.val();   
+                var $nbOfFloor = $(this).find("[name=NbFloor_Corp]");   //Scope the Nb of Floor
+                var nbOfFloor = $nbOfFloor.val();   
+                var nbOfStories = +nbOfFloor + +nbOfBasement;       //set to number
+                var totalOcc = occByFloor * nbOfStories;
+                var nbOfElevator = totalOcc / parseInt("1000", 10);
+                var nbOfColumn = nbOfStories / parseInt("20", 10);
+                var nbOfElevByColumn = Math.ceil(nbOfElevator) / Math.ceil(nbOfColumn);         // Ceiling numbers to higher decimal 
+                var totalElev = Math.ceil(nbOfElevByColumn) * Math.ceil(nbOfColumn) || 0;        // ceiling number and force but 0 instead of NaN  
+                var raw_total_Cost = Math.ceil(totalElev) * prod_line; // multiply
+                var roundedCost = raw_total_Cost.toFixed(2);        // fix 2 digits after comma
+                var total_Cost = Number(roundedCost);       // set to number
 
-            $("#elevator-needed").text(totalElev);
-            $("#cost-result").text(total_Cost + " $");
-            })
+                $("#elevator-needed").text(totalElev);
+                $("#cost-result").text(total_Cost + " $");   
+                })
 
-            /************  Generate the estimate Cost for Residential and the number of Elevator needed*********/
-            
-        $("#AptForm").on('change keyup', function(){
+                /*********** Generate the estimate Cost For Hybrid use and Number of Elevator needed  ************/
+
+            $("#HybForm").on("change keyup", function(){
+                var $occByFloor = $(this).find("[name=OccByFloor_Hyb]"); // Scope the Nb of Occupant By Floor
+                var occByFloor = $occByFloor.val();   
+                var $nbOfBasement = $(this).find("[name=NbBasement_Hyb]"); // Scope the Nb of Basement
+                var nbOfBasement = $nbOfBasement.val();   
+                var $nbOfFloor = $(this).find("[name=NbFloor_Hyb]");   //Scope the Nb of Floor 
+                var nbOfFloor = $nbOfFloor.val();  
+                var nbOfStories = +nbOfFloor + +nbOfBasement;       //set to number
+                var totalOcc = occByFloor * nbOfStories;
+                var nbOfElevator = totalOcc / parseInt("1000", 10) ; 
+                var nbOfColumn = nbOfStories / parseInt("20", 10) ;
+                var nbOfElevByColumn = Math.ceil(nbOfElevator) / Math.ceil(nbOfColumn);     // Ceiling numbers to higher decimal 
+                var totalElev = Math.ceil(nbOfElevByColumn) * Math.ceil(nbOfColumn) ||0;    // ceiling number and force but 0 instead of NaN         
+                var raw_total_Cost = totalElev * prod_line; // multiply
+                var roundedCost = raw_total_Cost.toFixed(2);        // fix 2 digits after comma
+                var total_Cost = Number(roundedCost);       // set to number
+
+                $("#elevator-needed").text(totalElev);
+                $("#cost-result").text(total_Cost + " $");
+                })
+
+                /************  Generate the estimate Cost for Residential and the number of Elevator needed*********/
+                
+            $("#AptForm").on('change keyup', function(){
             var $NbOfApart = $(this).find("[name=NbDoor_res]");
             var NbOfApart = $NbOfApart.val();
             var $NbOfFloor = $(this).find("[name=NbFloor_res]");
-            var NbOfFloor = $NbOfFloor.val();
+            var raw_NbOfFloor = $NbOfFloor.val();
+            var $NbBasements_res = $(this).find("[name=NbBasements_res]");
+            var NbBasements_res = $NbBasements_res.val();
+            var NbOfFloor = raw_NbOfFloor - NbBasements_res;
             var AvrgDoor = +NbOfApart / +NbOfFloor;
             var NbElev_res = Math.ceil(AvrgDoor) / 6; // Num of elevator
-            var NbColumn_res = NbOfFloor / parseInt("20", 10);
-            var NbRealElev_res = Math.ceil(NbElev_res) * Math.ceil(NbColumn_res) || 0 ;     // ceiling number and force but 0 instead of NaN  
+            var NbColumn_res = NbOfFloor / parseInt("20", 10) ;
+            var NbRealElev_res = Math.ceil(NbElev_res) * Math.ceil(NbColumn_res)  || 0 ;     // ceiling number and force but 0 instead of NaN  
             var raw_total_Cost = NbRealElev_res * prod_line;
             var roundedCost  = raw_total_Cost.toFixed(2);        // fix 2 digits after comma
             var total_Cost   = Number(roundedCost);       // set to number
-        
+            
             $("#elevator-needed").text(NbRealElev_res);
             $("#cost-result").text(total_Cost + " $");
-        })
+            
+
+            })
+        
+
     })
